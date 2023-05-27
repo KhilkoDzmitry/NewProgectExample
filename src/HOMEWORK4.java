22625139323ce55c22fe1ef62dbcae192308b912
User <14asz.xilko.d@pdu.by>
2023-05-27 20:33:27 +0300

    HOMEWORK_3

---
+++ src/HOMEWORK4.java
@@ -0,0 +1,15 @@
+public class HOMEWORK4 {
+
+public static void main(String[] args)
+       double s = sumX(x:10, y:4);
+       double z = sumY(x:2, y:10);
+       System.out.println(s > z);
+}
+public static double sumX (double x, double y)
+{
+    return x*y;
+}
+public static double sumY (double x, double y)
+{
+            return x*y;
+}

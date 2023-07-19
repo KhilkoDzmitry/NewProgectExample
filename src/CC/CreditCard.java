27987e268ec02093d8fcd812c3da810335acd0a6
User <14asz.xilko.d@pdu.by>
2023-07-19 16:34:47 +0300

    HOMEWORK7

---
+++ src/CC/CreditCard.java
@@ -0,0 +1,48 @@
+package CC;
+
+public class CreditCard {
+    private String accountNumber;
+    private double balance;
+
+    public CreditCard(String accountNumber, double balance)
+    {
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+    }
+
+    public void addMoney(double addSum)
+    {
+        if (addSum>=0)
+        {
+            this.balance+=addSum;
+        }else{
+            System.out.println("You cannot add this sum:"+addSum);
+        }
+
+    }
+    public double getBalance()
+    {
+        return this.balance;
+    }
+
+    public void withdraw(double withdrawSum)
+    {
+        if (withdrawSum<=this.balance);
+        {
+            this.balance-=withdrawSum;
+
+        }else{
+            System.out.println("You cannot withdraw the Sum:" + withdrawSum+". " +
+                    "Because you don`t have enough money. Your balance is:" + this.balance);
+    }
+    }
+    public String getCreditCardinfo()
+    {
+        return "Credit Card account Number:" +this.accountNumber +"\n" +
+                "Credit Card current balance is:" +this.balance +"\n";
+
+    }
+
+
+
+}
---
+++ src/CC/MyMain.java
@@ -0,0 +1,44 @@
+package CC;
+
+public class MyMain {
+    public static void main(String[] args) {
+        CreditCard tbc = new CreditCard("2356563289654563", 658);
+        CreditCard bog = new CreditCard("4562325645632569", 655);
+        CreditCard paysera = new CreditCard("1233211231111111", 900);
+
+        System.out.println(tbc.getCreditCardinfo());
+        System.out.println();
+        System.out.println(bog.getCreditCardinfo());
+        System.out.println();
+        System.out.println(paysera.getCreditCardinfo());
+        System.out.println();
+
+        tbc.addMoney(967);
+        bog.addMoney(20);
+        paysera.withdraw(52);
+
+        System.out.println();
+
+        System.out.println(tbc.getCreditCardinfo());
+        System.out.println();
+        System.out.println(bog.getCreditCardinfo());
+        System.out.println();
+        System.out.println(paysera.getCreditCardinfo());
+        System.out.println();
+        System.out.println("--------------------------------------------");
+        tbc.addMoney(-1000);
+        tbc.getBalance();
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
